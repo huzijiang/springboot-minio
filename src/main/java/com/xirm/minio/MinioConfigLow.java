bc4f4f651cf1cc81ab85be984a59cebe4d2af520
huzj <huzijiang@hqzhuanche.com>
2022-07-21 21:24:17 +0800

    init ....

--- src/main/java/com/xirm/minio/MinioConfigLow.java
+++ src/main/java/com/xirm/minio/MinioConfigLow.java
@@ -11,29 +11,29 @@ import org.springframework.context.annotation.Configuration;
  * @version 1.0
  * @date 2022/7/3 13:24
  */
-//@Configuration
+@Configuration
 public class MinioConfigLow {
 
-//    @Value("${fileserver.minio.server.host}")
-//    private String host;
-//
-//    @Value("${fileserver.minio.server.secure}")
-//    private boolean secure;
-//
-//    @Value("${fileserver.minio.server.port}")
-//    private int port;
-//
-//    @Value("${fileserver.minio.access-key}")
-//    private String accessKey;
-//
-//    @Value("${fileserver.minio.secret-key}")
-//    private String secretKey;
-//
-//    @Value("${fileserver.minio.region}")
-//    private String region;
-//
-//    @Bean
-//    public MinioClient minioClient() throws Exception {
-//        return new MinioClient(host, port, accessKey, secretKey, region, secure);
-//    }
+    @Value("${fileserver.minio.server.host}")
+    private String host;
+
+    @Value("${fileserver.minio.server.secure}")
+    private boolean secure;
+
+    @Value("${fileserver.minio.server.port}")
+    private int port;
+
+    @Value("${fileserver.minio.access-key}")
+    private String accessKey;
+
+    @Value("${fileserver.minio.secret-key}")
+    private String secretKey;
+
+    @Value("${fileserver.minio.region}")
+    private String region;
+
+    @Bean
+    public MinioClient minioClient() throws Exception {
+        return new MinioClient(host, port, accessKey, secretKey, region, secure);
+    }
 }
